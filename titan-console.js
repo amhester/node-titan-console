6aebd191341736ea28ff4f09bd89b4d423b5ff41
Professor X <amhester@anderson.edu>
2016-01-07 15:13:36 -0500

    Updated gremlin-client package

--- titan-console.js
+++ titan-console.js
@@ -5,15 +5,31 @@ var gremlin = require('gremlin-client');
 
 let port = argv.port || 8182;
 let host = argv.host || 'localhost';
-let query = argv.query || 'g.V()';
+let query = argv.query;
 
 var client = gremlin.createClient(port, host);
 
-client.execute(query, function(err, results) {
-    if (!err) {
-        console.log(results);
-    } else {
-        console.log(err);
-    }
-    process.exit(1);
-});
+if(query) {
+	sendQuery();
+} else {
+	console.log('Please enter query...');
+	process.stdin.resume();
+	process.stdin.once('data', function (data) {		
+		process.stdin.pause();
+		console.log('received query...');
+		query = data.toString().trim();
+		sendQuery();
+	});
+}
+
+function sendQuery() {
+	console.log('sending query...');
+	client.execute(query, function(err, results) {
+		if (!err) {
+			console.log(results);
+		} else {
+			console.log(err);
+		}
+		process.exit(1);
+	});	
+}
