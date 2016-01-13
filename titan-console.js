b91a7c0d333f4cf5cc5d26b54e73f1097e4d72f0
Professor X <amhester@anderson.edu>
2016-01-13 14:02:22 -0500

    made the client a repl

--- titan-console.js
+++ titan-console.js
@@ -11,15 +11,9 @@ var client = gremlin.createClient(port, host);
 
 if(query) {
 	sendQuery();
+	process.exit(1);
 } else {
-	console.log('Please enter query...');
-	process.stdin.resume();
-	process.stdin.once('data', function (data) {		
-		process.stdin.pause();
-		console.log('received query...');
-		query = data.toString().trim();
-		sendQuery();
-	});
+	promptQuery();
 }
 
 function sendQuery() {
@@ -30,6 +24,19 @@ function sendQuery() {
 		} else {
 			console.log(err);
 		}
-		process.exit(1);
+		if(!query) {
+			promptQuery();
+		}
 	});	
+}
+
+function promptQuery() {
+	console.log('Please enter query...');
+	process.stdin.resume();
+	process.stdin.once('data', function (data) {
+		process.stdin.pause();
+		console.log('received query...');
+		query = data.toString().trim();
+		sendQuery();
+	});
 }
