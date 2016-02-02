91722a583c19be49c06c3ea054d1a74764bb7ba8
Professor X <amhester@anderson.edu>
2016-02-02 09:42:55 -0500

    added a --help flag which displays the command options

--- titan-console.js
+++ titan-console.js
@@ -7,6 +7,18 @@ var Spinner = require('cli-spinner').Spinner;
 let port = argv.port || 8182;
 let host = argv.host || 'localhost';
 
+if(argv.help) {
+	console.log(
+		'\n' +
+		'\tThe available options for the TitanClient CLI tool are as follows:\n' +
+			'\t\t--port a number for the port you want to connect to (defaults to 8182)\n' +
+			'\t\t--host a string for the host address you want to connect to (defaults to localhost)\n' +
+			'\t\t--query a string that contains a query you wish to run against titan (if not provided, will enter REPL mode)\n' +
+		'\n\tYou can also pipe a txt file into this command which will then be used as the query to execute.'
+	);
+	process.exit(1);
+}
+
 var client = gremlin.createClient(port, host);
 
 if(argv.query) {
